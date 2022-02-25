79f0c6cc1ce5e2fa141c992efc676252d7fa1d27
Tom Sleeter <tom@sleeter.com>
2022-02-25 11:00:06 -0800

    Testing fix for IE11 poly compatibility

--- src/fuzzySearch.js
+++ src/fuzzySearch.js
@@ -3,7 +3,7 @@ import Fuse from 'fuse.js';
 
 export default function fuzzySearch(options) {
     const fuse = new Fuse(options, {
-        keys: ['name', 'groupName', 'items.name'],
+        keys: ['name'],
         threshold: 0.3,
     });
 
